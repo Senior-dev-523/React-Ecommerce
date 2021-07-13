8118ce87d027a211c494eaede26e115b01d5b48d
NiceToMeetYou0321 <soft.dev.talent.0321@gmail.com>
2021-07-14 04:39:34 +0800

    second commit

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Title from '../Title'
 import CartColumns from './CartColumns';
 import EmptyCart from './EmptyCart';
@@ -8,18 +8,18 @@ import CartTotals from './CartTotals';
 
 export default class Cart extends Component {
   render() {
-    return(
+    return (
       <section>
         <ProductConsumer>
           {value => {
             const { cart } = value;
-            if(cart.length > 0) {
+            if (cart.length > 0) {
               return (
                 <React.Fragment>
                   <Title name="your" title="cart" />
                   <CartColumns />
                   <CartList value={value} />
-                  <CartTotals value={value} />
+                  <CartTotals value={value} history={this.props.history} />
                 </React.Fragment>
               )
             } else {
--- src/components/Cart/CartTotals.js
+++ src/components/Cart/CartTotals.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import PayPalButton from './PayPalButton';
 
-export default function CartTotals({ value }) {
+export default function CartTotals({ value, history }) {
   const { cartSubTotal, cartTax, cartTotal, clearCart } = value;
   return (
     <React.Fragment>
@@ -10,7 +11,7 @@ export default function CartTotals({ value }) {
           <div className="col-10 mt-2 ml-sm-5 ml-md-auto col-sm-8 text-capitalize text-right">
             <Link to="/">
               <button className="btn btn-outline-danger text-uppercase mb-3 px-5" type="button"
-              onClick={() => clearCart()}>clear cart</button>
+                onClick={() => clearCart()}>clear cart</button>
             </Link>
             <h5>
               <span className="text-title">subtotal :</span>
@@ -24,6 +25,7 @@ export default function CartTotals({ value }) {
               <span className="text-title">total :</span>
               <strong>$ {cartTotal}</strong>
             </h5>
+            <PayPalButton total={cartTotal} clearCart={clearCart} history={history} />
           </div>
         </div>
       </div>
