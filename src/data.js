89832ddec15daefba9de7100852aa42a3c2b483b
NiceToMeetYou0321 <soft.dev.talent.0321@gmail.com>
2021-07-14 03:39:21 +0800

    first commit

---
+++ src/data.js
@@ -0,0 +1,102 @@
+export const storeProducts = [
+  {
+    id: 1,
+    title: "Google Pixel - Black",
+    img: "img/product-1.png",
+    price: 587,
+    company: "GOOGLE",
+    info: "The new unlocked Samsung Galaxy S7 is designed for the way you live, giving you access to most U.S. GSM and CDMA networks.1 And you can take it virtually anywhere you go, from Mexico City to Paris, and still stay connected.2 Plus you can rest easy with Samsung’s standard U.S. limited warranty.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 2,
+    title: "Samsung S7",
+    img: "img/product-2.png",
+    price: 145,
+    company: "SAMSUNG",
+    info: "The new unlocked Samsung Galaxy S7 is designed for the way you live, giving you access to most U.S. GSM and CDMA networks.1 And you can take it virtually anywhere you go, from Mexico City to Paris, and still stay connected.2 Plus you can rest easy with Samsung’s standard U.S. limited warranty.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 3,
+    title: "HTC 10 - Black",
+    img: "img/product-3.png",
+    price: 245.74,
+    company: "HTC",
+    info: "HTC 10. It’s more of what you’re looking for in a flagship phone. Unparalleled performance. Superb 24-bit Hi-Res sound. The world’s first* Optical Image Stabilization in both front and back cameras. And one of the highest smartphone camera rankings ever from DxOMark. All in a beautifully crafted metal unibody.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 4,
+    title: "HTC 10 - White",
+    img: "img/product-4.png",
+    price: 134.99,
+    company: "HTC",
+    info: "HTC 10. It’s more of what you’re looking for in a flagship phone. Unparalleled performance. Superb 24-bit Hi-Res sound. The world’s first* Optical Image Stabilization in both front and back cameras. And one of the highest smartphone camera rankings ever from DxOMark. All in a beautifully crafted metal unibody.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 5,
+    title: "HTC Desire 626s",
+    img: "img/product-5.png",
+    price: 219.99,
+    company: "HTC",
+    info: "The HTC Desire 626s is an Android-powered smartphone manufactured by HTC. It is available on Cricket (a subsidiary of AT&T), T-Mobile, MetroPCS (a T-Mobile subsidiary), Sprint Prepaid, and Virgin Mobile (a Sprint subsidiary) in the United States. The phone was announced in July 2015. It is considered the budget version of the Desire 626.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 6,
+    title: "Vintage Iphone",
+    img: "img/product-6.png",
+    price: 575,
+    company: "APPLE",
+    info: "This iPhone is in excellent condition as seen on the pictures. 100% working. Responsive LCD display, No dead pixels. All buttons, camera and WiFi working properly. Battery accepting charges, last long within charges. No Password, no iCloud account, clean IMEI, unlocked, not black-listed. The phone come in Apple1st Generation iPhone 2G for service thin box.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 7,
+    title: "Iphone 7",
+    img: "img/product-7.png",
+    price: 115,
+    company: "APPLE",
+    info: "An A10 Fusion chip for up to 2x faster performance than iPhone 6. Touch ID. A 4.7-inch Retina HD display with wide color and 3D Touch. Faster LTE. Immersive stereo sound, splash and water resistant, and iOS 10. The iPhone 7 display uses the same color space as the digital cinema industry, so what you see will be noticeably more brilliant and vibrant. When you use 3D Touch, your iPhone responds with subtle taps. The new Retina HD display deeply integrates 3D Touch throughout iOS. Now you can interact with Messages, Calendar, Mail, and other apps. We’ve reengineered that beloved camera, adding optical image stabilization, an ƒ/1.8 aperture, and a six-element lens to make it even better for shooting photos and videos in low light. And with advanced new features like wide color capture, your photos and Live Photos will look even more vibrant. With its entire enclosure reengineered, it is the very first water resistant iPhone. iPhone 7 is supercharged by the most powerful chip ever in a smartphone. It’s not just faster than any previous iPhone, it’s also more efficient. It enables faster processing when you need it and the ability to use even less power when you don’t. And with the longest battery life ever in an iPhone, you can work at twice the speed of iPhone 6 and still enjoy more time.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 8,
+    title: "Huawei P40 Lite",
+    img: "img/product-8.png",
+    price: 249.99,
+    company: "HUAWEI",
+    info: "A brand-new, unused, unopened, undamaged item in its original packaging (where packaging is applicable). Packaging should be the same as what is found in a retail store, unless the item is handmade or was packaged by the manufacturer in non-retail packaging, such as an unprinted box or plastic bag. See the seller's listing for full details",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+];
+
+export const detailProduct = {
+  id:1,
+  title: "Google Pixel - Black",
+  img: "img/product-1.png",
+  price: 10,
+  company: "google",
+  info: "Google Pixel is a brand of consumer electronic devices developed by Google that run either Chrome OS or the Android operating system. The Pixel brand was introduced in February 2013 with the first-generation Chromebook Pixel. The Pixel line includes laptops, tablets, and smartphones, as well as several accessories.",
+  inCart: false,
+  count: 0,
+  total: 0
+}
